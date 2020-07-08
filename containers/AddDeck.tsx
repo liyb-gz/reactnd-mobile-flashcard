4f868fe94c1c8d4b0bb3df6c90451609379a95f7
Felix Li <felix@FelixPro.local>
2020-07-08 22:56:40 +0800

    Add callback to addDeck view

--- containers/AddDeck.tsx
+++ containers/AddDeck.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { View, KeyboardAvoidingView } from 'react-native';
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
 import styles from '../styles/styles';
 import { Button, Input } from 'react-native-elements';
 import { tealBlue } from '../styles/colors';
+import { Routes, ModalStackProps } from '../ts/navigation';
 
-const AddDeck = () => {
+const AddDeck = ({ navigation }: ModalStackProps<Routes.AddDeck>) => {
+  const handleSubmit = useCallback(() => {
+    console.log('handleSubmit');
+    navigation.goBack();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Input
@@ -12,12 +18,14 @@ const AddDeck = () => {
         leftIcon={{ name: 'book', color: tealBlue }}
         inputContainerStyle={styles.addDeckTextInputContainer}
         inputStyle={styles.addDeckTextInput}
+        onSubmitEditing={handleSubmit}
       />
 
       <Button
         title="Add Deck"
         buttonStyle={styles.tealBlueButton}
         containerStyle={styles.buttonContainer}
+        onPress={handleSubmit}
       />
     </View>
   );
