4405dd207d42c74db917e5742c9c5b8ba3809886
Felix Li <felix@FelixPro.local>
2020-07-10 01:28:08 +0800

    add circular progress in Result

--- containers/Result.tsx
+++ containers/Result.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import ProgressCircle from 'react-native-progress/Circle';
 import styles from '../styles/styles';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { tealBlue } from '../styles/colors';
 
 const Result = () => {
   return (
     <View style={styles.container}>
       <Text>Result</Text>
-      {/* <ProgressCircle progress={0.3} color={tealBlue} /> */}
+
+      <AnimatedCircularProgress
+        size={120}
+        width={15}
+        fill={100}
+        tintColor="#00e0ff"
+        onAnimationComplete={() => console.log('onAnimationComplete')}
+        backgroundColor="#3d5875"
+      />
     </View>
   );
 };
