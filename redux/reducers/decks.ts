5f363852a988b1c1fe2019741078a1c8160e83be
Felix Li <felix@FelixPro.local>
2020-07-15 11:55:29 +0800

    use immer

--- redux/reducers/decks.ts
+++ redux/reducers/decks.ts
@@ -3,39 +3,60 @@ import {
   DeckReducer,
   DeckState,
   DeckActions,
+  Question,
 } from '../../ts/types';
 
 import ID from '../../utils/ID';
+import produce from 'immer';
 
 const decks: DeckReducer = (state: DeckState = {}, action: DeckActionTypes) => {
   switch (action.type) {
     case DeckActions.FetchDeck:
       return action.decks;
+
     case DeckActions.AddDeck:
-      const deckId = ID();
-      return {
-        ...state,
-        [deckId]: {
-          id: deckId,
-          title: action.deckName,
-          questions: [],
-        },
+      const newDeckId = ID();
+      const newDeck = {
+        id: newDeckId,
+        title: action.deckName,
+        questions: {},
       };
+      return produce(state, (draft) => {
+        draft[newDeckId] = newDeck;
+      });
+
+    case DeckActions.EditDeck:
+      return produce(state, (draft) => {
+        draft[action.deckId].title = action.deckName;
+      });
+
     case DeckActions.DeleteDeck:
-      const { [action.deckId]: deckToBeDeleted, ...rest } = state;
-      return rest;
+      return produce(state, (draft) => {
+        delete draft[action.deckId];
+      });
+
     case DeckActions.AddCard:
       const cardId = ID();
-      return {
-        ...state,
-        [action.deckId]: {
-          ...state[action.deckId],
-          questions: [
-            ...state[action.deckId].questions,
-            { ...action.question, id: cardId },
-          ],
-        },
+      const newQuestion: Question = {
+        ...action.question,
+        id: cardId,
       };
+      return produce(state, (draft) => {
+        draft[action.deckId].questions[cardId] = newQuestion;
+      });
+
+    case DeckActions.EditCard:
+      const { deckId: deckIdToBeEdited, question } = action;
+      return produce(state, (draft) => {
+        draft[deckIdToBeEdited].questions[question.id] = question;
+      });
+
+    case DeckActions.DeleteCard:
+      const { deckId: deckIdToBeDeleted, questionId } = action;
+      return produce(state, (draft) => {
+        delete draft[deckIdToBeDeleted].questions[questionId];
+      });
+
     default:
       return state;
   }
